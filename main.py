170857189759d9c482a6f5c19a384d0ec35941fb
kalebkohn <kjkohn@wisc.edu>
2024-09-18 21:30:18 -0500

    removed docker file since certain os commands weren't being recognized

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from bs4 import BeautifulSoup
+from PIL import Image, ImageDraw, ImageFont
 import requests
+import os
 import re
 
-def print_message_to_window():
-    pass
+img_path = "OIP.jpg"
+
+def print_to_window(img_path):
+    if os.name == 'nt':
+        print(os.getcwd())
+        print(img_path)
+        os.startfile(img_path)
+
+def add_text_to_image(img_path):
+        image = Image.open(img_path)
+        
+        # Create a Draw object
+        draw = ImageDraw.Draw(image)
+
+        font_path = "C:\\Windows\\Fonts\\arial.ttf"
+        font_size = 20
+        font = ImageFont.truetype(font_path, font_size)
+
+        # Positioning the text
+        text_position = (50, 30)
+
+        # Draw the text
+        draw.text(text_position, "New Grad Role Released!", fill="black", font=font)
+
+        # Save the modified image
+        image.save("new_grad.jpg")
 
 def main():
     r = requests.get("https://job-boards.greenhouse.io/vannevarlabs")
@@ -14,9 +40,11 @@ def main():
 
     for job in curr_openings:
         job = job.get_text().lower()
+        print(job)
 
         if  re.search(r'$new grad$', str(job)):
-            print_message_to_window()
+            add_text_to_image(img_path)
+            print_to_window("new_grad.jpg")
         else:
             print('no new grad roles found')
 
